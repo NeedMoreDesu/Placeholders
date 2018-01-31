c12162b40e1a488ba91627e31f89a6a91908100f
Oleksii Horishnii <o.horishnii@levi9.com>
2018-01-31 15:04:50 +0200

    separate files

--- Tablevc/TableViewContainer/TableContents.swift
+++ Tablevc/TableViewContainer/TableContents.swift
@@ -9,51 +9,6 @@
 import Foundation
 import UIKit
 
-public protocol TableViewCellGenerator {
-    var reuseId: String { get }
-    func registerReuseId(tableView: UITableView)
-    func updateCell(cell: UITableViewCell, vc: UIViewController)
-}
-
-extension CellGenerator.View: TableViewCellGenerator {
-    public func registerReuseId(tableView: UITableView) {
-        ContainersTableViewCell.registerReuseId(reuseId: self.reuseId, tableView: tableView)
-    }
-    
-    public func updateCell(cell: UITableViewCell, vc: UIViewController) {
-        if let cell = cell as? ContainersTableViewCell {
-            if cell.insertedView == nil {
-                cell.controllingVC = vc
-                cell.insertedView = self.create()
-            }
-            self.update(cell.insertedView!, vc)
-        }
-    }
-}
-
-extension CellGenerator.Xib: TableViewCellGenerator {
-    public func registerReuseId(tableView: UITableView) {
-        tableView.register(self.nib, forCellReuseIdentifier: self.reuseId)
-    }
-    
-    public func updateCell(cell: UITableViewCell, vc: UIViewController) {
-        self.update(cell, vc)
-    }
-}
-
-extension CellGenerator.Static: TableViewCellGenerator {
-    public func registerReuseId(tableView: UITableView) {
-        ContainersTableViewCell.registerReuseId(reuseId: self.reuseId, tableView: tableView)
-    }
-    
-    public func updateCell(cell: UITableViewCell, vc: UIViewController) {
-        if let cell = cell as? ContainersTableViewCell {
-            cell.controllingVC = vc
-            cell.insertedView = get(vc)
-        }
-    }
-}
-
 public protocol TableContents {
     func sections() -> Int
     func rows(section: Int) -> Int
---
+++ Tablevc/TableViewContainer/TableViewCellGenerator.swift
@@ -0,0 +1,55 @@
+//
+//  TableViewCellGenerator.swift
+//  Tablevc
+//
+//  Created by Oleksii Horishnii on 1/31/18.
+//  Copyright © 2018 Oleksii Horishnii. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol TableViewCellGenerator {
+    var reuseId: String { get }
+    func registerReuseId(tableView: UITableView)
+    func updateCell(cell: UITableViewCell, vc: UIViewController)
+}
+
+extension CellGenerator.View: TableViewCellGenerator {
+    public func registerReuseId(tableView: UITableView) {
+        ContainersTableViewCell.registerReuseId(reuseId: self.reuseId, tableView: tableView)
+    }
+    
+    public func updateCell(cell: UITableViewCell, vc: UIViewController) {
+        if let cell = cell as? ContainersTableViewCell {
+            if cell.insertedView == nil {
+                cell.controllingVC = vc
+                cell.insertedView = self.create()
+            }
+            self.update(cell.insertedView!, vc)
+        }
+    }
+}
+
+extension CellGenerator.Xib: TableViewCellGenerator {
+    public func registerReuseId(tableView: UITableView) {
+        tableView.register(self.nib, forCellReuseIdentifier: self.reuseId)
+    }
+    
+    public func updateCell(cell: UITableViewCell, vc: UIViewController) {
+        self.update(cell, vc)
+    }
+}
+
+extension CellGenerator.Static: TableViewCellGenerator {
+    public func registerReuseId(tableView: UITableView) {
+        ContainersTableViewCell.registerReuseId(reuseId: self.reuseId, tableView: tableView)
+    }
+    
+    public func updateCell(cell: UITableViewCell, vc: UIViewController) {
+        if let cell = cell as? ContainersTableViewCell {
+            cell.controllingVC = vc
+            cell.insertedView = get(vc)
+        }
+    }
+}
