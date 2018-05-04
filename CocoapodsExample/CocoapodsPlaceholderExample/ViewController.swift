0ca51b0edeecc7fc9c13138a1ad21fb9a5ffe009
Oleksii Horishnii <o.horishnii@levi9.com>
2018-05-04 16:46:34 +0300

    Cocoapods example project

---
+++ CocoapodsExample/CocoapodsPlaceholderExample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CocoapodsPlaceholderExample
+//
+//  Created by Oleksii Horishnii on 5/4/18.
+//  Copyright © 2018 Oleksii Horishnii. All rights reserved.
+//
+
+import UIKit
+import Placeholders
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var placeholder: PlaceholderView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel(frame: CGRect.zero)
+        label.text = "Placeholder cocoapods example"
+        self.placeholder.insertedView = label
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
