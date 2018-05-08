ac586e5eda6fde937bdb121a4d469b60e75d70d1
Oleksii Horishnii <o.horishnii@levi9.com>
2018-05-08 18:00:58 +0300

    ExampleCoreData (WIP)

---
+++ ExampleCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleCoreData
+//
+//  Created by Oleksii Horishnii on 5/8/18.
+//  Copyright © 2018 Oleksii Horishnii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
